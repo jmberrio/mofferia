3606b4f33780cd3328446bdbaf8f55b9911a2c21
Pilar Fernández <pilar.fernandez@euigs.com>
2021-04-08 23:39:37 +0200

    Persistencia

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,8 +1,14 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-        
+        apiKey: "AIzaSyBBSMF0VeKljmLOBMrSrhyOYfkjPV8VzUM",
+        authDomain: "mofferia-4eeaa.firebaseapp.com",
+        databaseURL: "https://mofferia-4eeaa-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "mofferia-4eeaa",
+        storageBucket: "mofferia-4eeaa.appspot.com",
+        messagingSenderId: "911028450077",
+        appId: "1:911028450077:web:05e117f95189a42562e7f4"
     },
-    apiRestUrl: 'https://mofferia-default-rtdb.firebaseio.com/',
-    apiKey: 'AIzaSyA7RqqMzvmvjzgTZNl5H3QyPn-hejJnujs'
+    apiRestUrl: 'https://mofferia-4eeaa-default-rtdb.europe-west1.firebasedatabase.app',
+    apiKey: 'AIzaSyBBSMF0VeKljmLOBMrSrhyOYfkjPV8VzUM'
 }
